546064aeb727433a77bf535dc2437d272465ddfa
Javier <1javier.ha@gmail.com>
2022-09-21 22:07:21 -0400

    Creacion Scripts, CustomerToTable, TableOcuped, SpawnCustomer

---
+++ Assets/Scripts/SpawnCustomer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCustomer : MonoBehaviour
+{
+    public bool tableFree;
+    public bool allTablesOcuped;
+
+    public GameObject[] customers;
+
+
+    private void Start()
+    {
+        tableFree = true;
+        allTablesOcuped = false;
+    }
+
+    void Update()
+    {
+        if (GameObject.Find("Table0").GetComponent<TableOcuped>().isOcuped==true && GameObject.Find("Table1").GetComponent<TableOcuped>().isOcuped == true && GameObject.Find("Table2").GetComponent<TableOcuped>().isOcuped == true && GameObject.Find("Table3").GetComponent<TableOcuped>().isOcuped == true && GameObject.Find("Table4").GetComponent<TableOcuped>().isOcuped == true)
+        {
+            allTablesOcuped = true;
+        }
+        else
+        {
+            allTablesOcuped = false;
+        }
+
+        if (tableFree == true && allTablesOcuped == false)
+        {
+            tableFree = false;
+            StartCoroutine(Asd());
+        }
+
+    }
+
+    IEnumerator Asd()
+    {
+        Instantiate(customers[UnityEngine.Random.Range(0, 1)], new Vector2(0, 4), Quaternion.identity);
+        yield return new WaitForSeconds(5);
+        tableFree = true;
+        
+    }
+}
